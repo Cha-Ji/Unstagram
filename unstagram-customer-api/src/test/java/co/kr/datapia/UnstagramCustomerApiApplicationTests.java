50967c452e91d9c3a82d39e20c3eb8dfeefdc5b0
차지원 <chajiwon100785@gmail.com>
2021-01-19 17:50:51 +0900

    init

---
+++ unstagram-customer-api/src/test/java/co/kr/datapia/UnstagramCustomerApiApplicationTests.java
@@ -0,0 +1,15 @@
+package co.kr.datapia;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UnstagramCustomerApiApplicationTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
